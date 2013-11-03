ceaccc83d8cd278e9d847b2d9e5fe567d0346b31
Andy <mail@andrewgriffithsonline.com>
2013-11-03 11:17:53 +0000

    add pavarotti

---
+++ js/vidz.js
@@ -0,0 +1,26 @@
+var vidz = angular.module('youtube-responsive', []);
+
+vidz.directive('responsiveVideo', function($rootScope) {
+    return {
+      templateUrl: '/tpl/youtube-video.html',
+      restrict: 'EA',
+      scope: {
+        videoSlug: '@'
+      },
+      replace: true,
+      link: function postLink($scope, element, attrs) {
+
+        $scope.getWidth = function() {
+          return parseInt(element.css('width'), 10) * (9/16);
+        };
+
+        $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+          $scope.height = newValue;
+        });
+
+        $(window).on('resize onorientationchange', $.throttle(250, function() {
+          $scope.$apply();
+        }));
+      }
+    };
+  });
